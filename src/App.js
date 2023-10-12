b1aa9e603b3a02df3dc2e62a2a7253e5c10ee74f
Irina <ivol-ga@ya.ru>
2023-10-12 11:18:15 +0300

    ReactBankApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { setData as setDataFromRedux } from './redux-state/reducers/data'
+import { Routes, Route } from 'react-router-dom'
+import Head from './components/views/global/Head'
+import Main from './components/pages/Main'
+import Stat from './components/pages/Stat'
+import Plan from './components/pages/Plan'
 
 function App() {
+
+  const [ ,setShowPage ] = useState('main')
+
+  const data = useSelector(state => state.dataReducer.data)
+  const dispatch = useDispatch()
+
+  const setData = (param) => dispatch(setDataFromRedux(param))
+
+  useEffect(() => {
+
+    ( async () => {
+
+      const data = await fetch('/get-data', {
+        method: 'POST',
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+      }).then(response => response.json())
+
+      data.data.forEach(item => dispatch(setDataFromRedux(item)))
+
+    })()
+
+  }, [ dispatch ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <Head action={setShowPage}></Head>
+      <Routes>
+        <Route
+          path={'/main'}
+          element={<Main action={setData}/>}
+        />
+        <Route
+          path={'/stat/:viewType'}
+          element={<Stat statData={data}/>}
+        />
+        <Route
+          path={'/plan'}
+          element={<Plan statData={data}/>}
+        />
+        <Route
+          path={'*'}
+          element={<Main action={setData}/>}
+        />
+      </Routes>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
